ca0fd977bb39cedb3c7d0c5d737845f868232042
Beyda Fentanes <beyfenta@gmail.com>
2024-01-03 20:23:27 -0600

    Ran bundle cloudinay

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,11 @@
 class State < ApplicationRecord
+
+  # Geocoder
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  # Cloudinary
+
+  has_one_atthached :photo
 end
